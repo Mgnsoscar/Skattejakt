51994ba4eb1504c6bb72851a716d9e8d19eb352a
Magnus Oscar Støleggen <mgnsoscar@gmail.com>
2022-11-24 23:32:33 +0100

    sdfg

--- Resources/Code/App.py
+++ Resources/Code/App.py
@@ -1,31 +1,59 @@
+import time
+
 from Resources.Code.customQtWidgets import *
+from PyQt5 import QtWidgets as qw, QtGui as qg, QtCore as qc, QtTest as qt
 
 class Main(mywindow):
 
     def __init__(self, *args, **kwargs):
 
         mywindow.__init__(self, *args, **kwargs)
+        self.max_restore()
         # Add Background Picture
         self.blurframe.addstyle('image','border-image: url("Resources/Images/background.jpg");')
 
-        self.frame = myframe(self.mainframe["2"], "v", "frame", add=True, color=(255, 255, 255, 100), radius=10)
-        self.frame.setFixedSize(0,0)
+        self.frame = myframe(self.mainframe["2"], "v", "frame", color=(255, 255, 255, 100), radius=10)
+        self.mainframe["2"].lay.addWidget(self.frame)
+        self.frame.setMaximumSize(0,0)
+        self.label = mylabel(self.frame, "label", align="center", add=True)
 
-        self.animation = Animation(lambda: self.opening())
-        self.animation.start()
-
-        self.currentx = 0
-        self.currenty = 0
+        self.message1 = "For mange hundre år siden var denne byen et knutepunkt for vikinger som reiste nordfra mot" \
+                        "mot england for å plyndre."
 
 
-    def opening(self):
 
-        if self.currenty <= 900 and self.currentx <=1400:
-            self.frame.setFixedSize(self.currentx, self.currenty)
-            self.currentx +=56
-            self.currenty +=36
-        else:
-            self.animation.stop()
+        #self.openingAnimation()
+
+        texx = Animation(lambda: self.aniText())
+        #texx.start()
+        texx.start()
+
+
+    def openingAnimation(self):
+
+        # Frame pops up
+        qt.QTest.qWait(1000)
+        popup = qc.QPropertyAnimation(self.frame, b'maximumSize')
+        popup.setEndValue(qc.QSize(int(self.mainframe["2"].width()*0.8), int(self.mainframe["2"].height()*0.8)))
+        popup.setEasingCurve(qc.QEasingCurve.OutBounce)
+        popup.setDuration(1200)
+        popup.start()
+        self.popup = popup
+
+    def aniText(self):
+
+        print("h")
+
+
+
+
+
+
+
+
+
+
+
 
 
 
